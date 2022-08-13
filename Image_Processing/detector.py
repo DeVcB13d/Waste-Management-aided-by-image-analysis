d98e038c15980debf7edb3afa3529c79c532e072
C B Dev Narayan <devcb@pg.cusat.ac.in>
2022-08-13 07:28:25 +0530

    Added docs

--- Image_Processing/detector.py
+++ Image_Processing/detector.py
@@ -1,40 +1,51 @@
 import cv2 as cv
 import numpy as np
-# Loading the image
 
-# empty_cup = cv.imread('Image_Processing\Trainset\canEmp.jpg')
-# filled_cup = cv.imread('Image_Processing\Trainset\canFill.jpg')
-
+'''
+DepthDetector class : Has functions that would aid in findexing the depth
+'''
 
 class DepthDetector:
     def __init__(self):
         pass
-
+    '''
+    Preprocessing 
+    >>> Aim is to findex the image edges for the detection algorithm to work
+    >>> The image after processing would return a 2 pixel image with 255 for the edges
+    '''
     def preprocess(self,image):
         #Reducing the size
         half = cv.resize(image, (0, 0), fx = 0.1, fy = 0.1)
         #Converting to grayscale
         gray = cv.cvtColor(half,cv.COLOR_BGR2GRAY)
+        #Reducing the noise in the image
         blur = cv.GaussianBlur(gray,(7,7),cv.BORDER_DEFAULT)
         #Adding a mask
         mask = cv.adaptiveThreshold(blur, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 19, 5)
-        #Finding contours
-        contours,_ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        # Findexing contours
+        # contours,_ = cv.findexContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         return mask
 
     # Function to calculate the clustring of white pixels of an array
     def getclusters(self,Array,deviation = 0):
+        '''
+        >>> On analyzing the image verically, we can see the edge pixels would clutter
+        >>> Using this function the starting indexex of the cluster and the amount of pixels that
+            cluster after that indexex would be returned
+        >>> Array : input image 
+        >>> Deviation : Pixels to move away from the central line
+        '''
         Array = np.array(Array)
         l = Array[:,int(Array.shape[1]/2) + deviation]
         white = []
         countWhite = 0
-        for ind,val in enumerate(l):   
+        for index,val in enumerate(l):   
             try:
                 if (val == 255) :  
-                    if(l[ind+1] != 0):
+                    if(l[index+1] != 0):
                         countWhite = countWhite + 1
                     else:
-                        white.append([ind,countWhite])
+                        white.append([index,countWhite])
                         countWhite = 0
             except:
                 pass
--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # Waste-Management-aided-by-image-analysis
 
+# Brief description
 1. Capacity Monitoring of Waste containers using Image based data
-❏ Our model would monitor the capacity of waste containers using images obtained at regular
-intervals
-❏ The images obtained are processed using a model trained to detect the container capacities
-from image data
+    * Our model would monitor the capacity of waste containers using images obtained at regular
+      intervals
+* The images obtained are processed using a model trained to detect the container capacities
+    from image data
 2. Container management system
-● Based on the data collected from images
-● Develop a model that would alert both the notifiers and collectors based on their requirements
+    * Based on the data collected from images
+    * Develop a model that would alert both the notifiers and collectors based on their requirements
 3. Analysis using the image based data
-❏ Using the image based data obtained to for further analysis on the waste disposed such as :
-❏ Materials used for disposal
-❏ Producing a report based on the collected data
+    * Using the image based data obtained to for further analysis on the waste disposed such as :
+    * Materials used for disposal
+    * Producing a report based on the collected data
+
